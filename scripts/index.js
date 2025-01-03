9d7022909eecdd2c7453e37fabd6b48834581f40
Elizaveta Zutikova <zutikovae@yandex.ru>
2025-01-03 15:09:40 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const placeList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
+function addCard(element, deleteCard) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(element) {
+    const deleteCard = element.target.closest('.card');
+    deleteCard.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (element) {
+    const card = addCard(element, deleteCard);
+    placeList.append(card);
+});
